61278b2199d74b07d67f00ff6c26eb2ac725afa7
James Fincher <heartandkarma@gmail.com>
2022-05-21 02:45:51 +0000

    Initial commit

---
+++ username_generator.py
@@ -0,0 +1,22 @@
+print("Hello! I am here to generate a slick username for you!!! " )
+
+animal = input("\n Whats your favorate animal? ")
+
+color = input(" \n That's a great animal! Next, what is your favorate color? ")
+
+number = input (""" \n“To see in color is a delight for the eye but to see in black and white is a delight for the soul.”
+
+- Andri Cauldwell.
+                
+ \nGreat color choice! \n \nOne last question.... What is your favorate number? """)
+
+print("\n Awesome! After some crazy calulations, I have decided to call you by your new username, \n ")
+
+new_username = color + animal + number
+
+print(new_username)
+
+print("\n \nI hope you come again " + new_username + "! This was fun :)")
+
+
+
